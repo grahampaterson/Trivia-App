a0d89f58bbb0dd60b7f275bef319716054347af8
grahampaterson <paterson.graham@gmail.com>
2017-04-03 22:49:31 +0200

    functioned script
    sorted script into functions and get everything functional

--- questions/trivia-playing-scrape.py
+++ questions/trivia-playing-scrape.py
@@ -4,32 +4,59 @@ import sys
 from bs4 import BeautifulSoup
 import requests
 
-base = "http://www.triviaplaying.com/"
-
 # get all the links to the different trivia pages
-page = requests.get('http://www.triviaplaying.com/include/rightside.html')
-html = page.content
+# and return list of urls
+def scrape_urls():
+    urls = []
+    page = requests.get('http://www.triviaplaying.com/include/rightside.html')
+    html = page.content
 
-soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-link = soup.find_all("a")
+    link = soup.find_all("a")
 
-# iterate over urls from home page
-# for a in link:
-#     print(a.attrs['href'])
+    # iterate over urls from home page and append to list
+    for a in link:
+        urls.append(a.attrs['href'])
 
-f = open("quiz.txt", "w")
+    return urls
+
 
 # parse document for paragraph tags containing questions
-# print(base + link[0].attrs['href'])
-quiz = requests.get(base + link[0].attrs['href'])
-quiz_content = quiz.content
-# print(quiz_content)
-quiz_soup = BeautifulSoup(quiz_content, 'html.parser')
-# print(quiz_soup)
-trivia = quiz_soup.find_all("p")
+def parse_questions(url):
 
-for q in trivia:
-    print(q.contents)
+    all_questions = []
 
-f.close()
+    base = "http://www.triviaplaying.com/"
+
+    # get url for quiz and scrape p tags
+    quiz = requests.get(base + url)
+    # quiz_content = quiz.content
+    quiz_soup = BeautifulSoup(quiz.content, 'html.parser')
+    trivia = quiz_soup.find_all("p")
+
+    # loop through paragraph tags finding questions and answers
+    for line in trivia:
+        if len(line.contents) > 1:
+            q = line.contents[0].string
+            # strip newline
+            q = q.replace("\n", "")
+            q = q.replace("\r", "")
+            q = q.replace("\r\n", "")
+
+            a = line.contents[1].string[4:]
+
+            all_questions.append({"q": q, "a": a})
+
+    return all_questions
+
+sample = [{"q": "question", "a": "answer"}]
+
+# takes a list of dicts with keys "q" and "a" and writes them to file quiz.txt
+def write_file():
+    f = open("quiz.txt", "w")
+    for line in sample:
+        f.write(line["q"] + line["a"])
+    f.close
+
+# parse_questions(link[0].attrs['href'])
